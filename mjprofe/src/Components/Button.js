5b5120b1d812f72b8d744ff1a176b6583250eff0
Raj Kumar <Raj.Kumar@mjunction.in>
2023-10-10 12:45:12 +0530

    changed

--- mjprofe/src/Components/Button.js
+++
@@ -1,8 +0,0 @@
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-const Button=(props)=>{
-
-    return (
-        <button type={submit} id={props.id}>{props.value}</button>
-    );
-}
