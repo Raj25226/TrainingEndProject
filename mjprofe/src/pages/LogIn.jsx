2b32cb227f726d0f1e6e93e5a29d4f30c17f3360
Raj Kumar <Raj.Kumar@mjunction.in>
2023-10-10 14:33:57 +0530

    bootstrap-added

--- mjprofe/src/pages/LogIn.jsx
+++ mjprofe/src/pages/LogIn.jsx
@@ -1,3 +1,49 @@
-export default function LogIn() {
-    return <h1>LogIn Page</h1>;
-}
+import React, { useState } from 'react';
+
+const LogIn = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    // Add your authentication logic here
+    // You may make an API request to your server to validate the credentials
+
+    // For simplicity, let's just log the credentials for now
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LogIn;
