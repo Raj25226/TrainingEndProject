b0df14fa5f7e4b443e6a51375566d2ebc96bbd61
Raj Kumar <Raj.Kumar@mjunction.in>
2023-10-10 15:06:55 +0530

    style.css

--- mjprofe/src/pages/LogIn.jsx
+++ mjprofe/src/pages/LogIn.jsx
@@ -1,49 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const LogIn = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleLogin = async () => {
-    // Add your authentication logic here
-    // You may make an API request to your server to validate the credentials
+    const handleLogin = async () => {
+        // Add your authentication logic here
+        // You may make an API request to your server to validate the credentials
 
-    // For simplicity, let's just log the credentials for now
-    console.log('Username:', username);
-    console.log('Password:', password);
+        // For simplicity, let's just log the credentials for now
+        console.log("Username:", username);
+        console.log("Password:", password);
 
-    setUsername('');
-    setPassword('');
-  };
+        setUsername("");
+        setPassword("");
+    };
 
-  return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="button" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="card-title mb-4">Log In</h2>
+                            <form>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="username"
+                                        className="form-label"
+                                    >
+                                        Username:
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="username"
+                                        value={username}
+                                        onChange={(e) =>
+                                            setUsername(e.target.value)
+                                        }
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label
+                                        htmlFor="password"
+                                        className="form-label"
+                                    >
+                                        Password:
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        value={password}
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
+                                </div>
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    onClick={handleLogin}
+                                >
+                                    Login
+                                </button>
+                                <div className="mt-3">
+                                    <a href="/forgot-password">
+                                        Forgot Password?
+                                    </a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LogIn;
