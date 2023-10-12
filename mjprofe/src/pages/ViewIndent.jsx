b144ead297ce3c426afb04648d70daf6f20441e0
Gurdeep Singh <Gurdeep.Singh@mjunction.in>
2023-10-12 23:13:44 +0530

    view

--- mjprofe/src/pages/ViewIndent.jsx
+++ mjprofe/src/pages/ViewIndent.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const ViewIndents = (props) => {
-  const [indents, setIndents] = useState({});
+  const [indents, setIndents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -11,7 +11,15 @@ const ViewIndents = (props) => {
         const apiEndpoint = `http://localhost:8080/mj/indent1/${props.headerId}`;
         const response = await axios.get(apiEndpoint);
         const fetchedIndents = response.data;
-        setIndents(fetchedIndents);
+
+        if (Array.isArray(fetchedIndents)) {
+          setIndents(fetchedIndents);
+        } else if (typeof fetchedIndents === "object") {
+          setIndents([fetchedIndents]);
+        } else {
+          console.error("Unexpected data format:", fetchedIndents);
+        }
+
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching indents:", error);
@@ -28,14 +36,40 @@ const ViewIndents = (props) => {
       ) : (
         <div className="card mb-3" style={{ background: "#176B87", color: "#EEEEEE" }}>
           <div className="card-body">
-            <h5 className="card-title">Indent:</h5>
-            {indents.indentId && <p>IndentId: {indents.indentId}</p>}
-            {indents.indentCode && <p>IndentCode: {indents.indentCode}</p>}
-            {indents.unitPrice && <p>UnitPrice: {indents.unitPrice}</p>}
-            {indents.totalPrice && <p>Totalprice: {indents.totalPrice}</p>}
-            {indents.quantity && <p>Quantity: {indents.quantity}</p>}
-            {/* {indents.indentcategory && <p>Category: {indents.indentcategory}</p>} */}
-            {/* Check for the existence of 'indentcategory' */}
+            <h5 className="card-title">Indents:</h5>
+            {Array.isArray(indents) ? (
+              indents.map((indent, index) => (
+                <div key={index}>
+                  {indent.indentId && <p>IndentId: {indent.indentId}</p>}
+                  {indent.indentCode && <p>IndentCode: {indent.indentCode}</p>}
+                  {indent.unitPrice && <p>UnitPrice: {indent.unitPrice}</p>}
+                  {indent.totalPrice && <p>Totalprice: {indent.totalPrice}</p>}
+                  {indent.quantity && <p>Quantity: {indent.quantity}</p>}
+                  {indent.createdAt && <p>CreatedAt: {indent.createdAt}</p>}
+                  {indent.indentHeaderVO?.indentHeaderId && (
+                    <p>IndentheadeId: {indent.indentHeaderVO.indentHeaderId}</p>
+                  )}
+                  {indent.indentHeaderVO?.netprice && (
+                    <p>Netprice: {indent.indentHeaderVO.netprice}</p>
+                  )}
+                </div>
+              ))
+            ) : (
+              <div>
+                {indents.indentId && <p>IndentId: {indents.indentId}</p>}
+                {indents.indentCode && <p>IndentCode: {indents.indentCode}</p>}
+                {indents.unitPrice && <p>UnitPrice: {indents.unitPrice}</p>}
+                {indents.totalPrice && <p>Totalprice: {indents.totalPrice}</p>}
+                {indents.quantity && <p>Quantity: {indents.quantity}</p>}
+                {indents.createdAt && <p>CreatedAt: {indents.createdAt}</p>}
+                {indents.indentHeaderVO?.indentHeaderId && (
+                  <p>IndentheadeId: {indents.indentHeaderVO.indentHeaderId}</p>
+                )}
+                {indents.indentHeaderVO?.netprice && (
+                  <p>Netprice: {indents.indentHeaderVO.netprice}</p>
+                )}
+              </div>
+            )}
           </div>
         </div>
       )}
