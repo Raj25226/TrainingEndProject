e2654436c5b9ce771ff4839bc3d1f3b4714e104d
Raj Kumar <Raj.Kumar@mjunction.in>
2023-10-30 10:13:05 +0530

    tomcat added

--- mjprolite/src/main/java/com/mj/MjproliteApplication.java
+++ mjprolite/src/main/java/com/mj/MjproliteApplication.java
@@ -3,9 +3,10 @@ package com.mj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-public class MjproliteApplication {
+public class MjproliteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MjproliteApplication.class, args);
